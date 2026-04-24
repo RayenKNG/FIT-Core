9028c945c50de06fa6e5caafb6e7840c82f4335e
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-04-24 09:43:48 +0700

    menambah code pada login_page.dart

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,37 +9,27 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fitcore_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+    namespace "com.example.fitcore_app"
+    compileSdkVersion 34
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fitcore_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId "com.example.fitcore_app"
+        minSdkVersion 21
+        targetSdkVersion 34
+        versionCode 1
+        versionName "1.0.0"
+        multiDexEnabled true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.debug
         }
     }
+}
+
+dependencies {
+    implementation 'com.google.firebase:firebase-bom:33.0.0'
 }
 
 flutter {
