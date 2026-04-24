f7a6a94c24558ec2ea606c1ac92afee372df9c6e
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-04-24 07:51:25 +0700

    auth_repository.dart

---
+++ lib/features/auth/domain/repositories/auth_repository_impl.dart
@@ -0,0 +1,17 @@
+// Sesuai modul hal.47
+import 'package:dio/dio.dart';
+import '../../../../core/constants/api_constants.dart';
+import '../../../../core/services/dio_client.dart';
+import 'auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<String> verifyFirebaseToken(String firebaseToken) async {
+    final response = await DioClient.instance.post(
+      ApiConstants.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+    final data = response.data['data'] as Map<String, dynamic>;
+    return data['access_token'] as String;
+  }
+}
